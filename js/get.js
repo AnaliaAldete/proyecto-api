5778a098d4c5e6fcc2b11d7151bb5572bfed0004
Analia <ana_292_1@hotmail.com>
2024-04-24 21:05:05 -0300

    estile ver datalle

--- js/get.js
+++ js/get.js
@@ -24,7 +24,7 @@ const renderCard = (data) => {
 					<img src="${url}"alt="imagen pelicula">
 					<div class="container-descripcion">
 						<h2>${name}</h2>
-						<button class="btn-ver-detalle">Ver detalle</button>
+						<button class="btn-ver-detalle" data-cardId="${id}">Ver detalle</button>
 					</div>
 				</div> 
     `;
@@ -42,12 +42,57 @@ const ocultarSpinner = () => containerSpinner.classList.add("hidden");
 
 const asignarEventoVerDetalle = (btns) => {
 	btns.forEach((btn) =>
-		btn.addEventListener("click", () => {
-			console.log("click");
+		btn.addEventListener("click", (e) => {
+			getDetalle(btn.getAttribute("data-cardId"));
 		})
 	);
 };
 
-// const verDetalle=(idPeli)=>{
+const getDetalle = (idPeli) => {
+	fetch(`${baseUrl}/${idPeli}`)
+		.then((res) => res.json())
+		.then((data) => renderDetallePeli(data))
+		.catch((err) => console.log(err));
+};
 
-// }
+const renderDetallePeli = (data) => {
+	renderSpinner();
+	setTimeout(() => {
+		ocultarSpinner();
+		ContainerCards.innerHTML = "";
+
+		const {
+			name,
+			url,
+			id,
+			descripcion,
+			anio,
+			genero,
+			director,
+			actoresPrincipales,
+			calificacion,
+		} = data;
+		ContainerCards.innerHTML = `
+     <div id="card-detalle" class="card-detalle">
+     
+					<img src="${url}" alt="imagen pelicula">
+					<div class="container-detalle">
+                    <div>
+                    <button>X</button>
+                    </div>
+						<h2>${name}</h2>
+                        <p>${descripcion}</p>
+                        <p>${anio}</p>
+                        <p>${genero}</p>
+                        <p>${director}</p>
+                        <p>${actoresPrincipales}</p>
+                        <p>${calificacion}</p>
+                        <div>
+						<button class="btn-editar-peli" data-cardId="${id}">Editar</button>
+						<button class="btn-eliminar-peli" data-cardId="${id}">Eliminar</button>
+                        </div>
+					</div>
+                    
+				</div>`;
+	});
+};
