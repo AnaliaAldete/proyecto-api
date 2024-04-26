90c24b45a3fb634e7c648a2a6a73801e563a1a26
Analia <ana_292_1@hotmail.com>
2024-04-26 19:51:26 -0300

    agregue form y su funcionalidad

--- js/get.js
+++ js/get.js
@@ -55,7 +55,7 @@ const getDetalle = (idPeli) => {
 		.catch((err) => console.log(err));
 };
 
-const renderDetallePeli = (data) => {
+const renderDetallePeli = (peli) => {
 	renderSpinner();
 	setTimeout(() => {
 		ocultarSpinner();
@@ -71,7 +71,7 @@ const renderDetallePeli = (data) => {
 			director,
 			actoresPrincipales,
 			calificacion,
-		} = data;
+		} = peli;
 		ContainerCards.innerHTML = `
     <div id="card-detalle" class="card-detalle">
 	    <img src="${url}" alt="imagen pelicula" />
@@ -87,12 +87,83 @@ const renderDetallePeli = (data) => {
 		    <p>Actores principales: ${actoresPrincipales}</p>
 		    <p>Calificación: ${calificacion}</p>
 		    <div>
-			    <button class="btn-editar-peli" data-cardId="${id}">Editar</button>
+			    <button class="btn-editar-peli" id="btn-editar-peli" data-cardId="${id}">Editar</button>
 			    <button class="btn-eliminar-peli" data-cardId="${id}">Eliminar</button>
 		    </div>
 	    </div>
-    </div>`;
-		const btnX = document.getElementById("btnX");
-		btnX.addEventListener("click", getPeliculas);
+    </div>
+
+   <form class="hidden" id="form-editar-peli">
+				<div>
+					<label for="input-nombre">Pelicula</label>
+					<input type="text" name="nombre" id="input-nombre" />
+				</div>
+				<div>
+					<label for="input-descripcion">Sinopsis</label>
+					<input type="text" name="descripcion" id="input-descripcion" />
+				</div>
+				<div>
+					<label for="input-anio">Lanzamiento</label>
+					<input type="number" name="anio" id="input-anio" />
+				</div>
+				<div>
+					<label for="input-genero">Genero</label>
+					<input type="text" name="genero" id="input-genero" />
+				</div>
+				<div>
+					<label for="input-director">Director</label>
+					<input type="text" name="director" id="input-director" />
+				</div>
+				<div>
+					<label for="input-actores">Actores principales</label>
+					<input type="text" name="actores" id="input-actores" />
+				</div>
+				<div>
+					<label for="input-calificacion">Calificación</label>
+					<input type="text" name="calificacion" id="input-calificacion" />
+				</div>
+				<div>
+					<button type="submit">Editar</button>
+					<button id="btn-cancelar-edicion">Cancelar</button>
+				</div>
+			</form>
+    `;
+		document.getElementById("btnX").addEventListener("click", getPeliculas);
+
+		document.getElementById("btn-editar-peli").addEventListener("click", () => {
+			mostrarFormEditar(peli);
+		});
+		document
+			.getElementById("btn-cancelar-edicion")
+			.addEventListener("click", (e) => {
+				e.preventDefault();
+				cancelarEdicion();
+			});
 	}, 2000);
+};
+
+const mostrarFormEditar = (peli) => {
+	const inputName = document.getElementById("input-nombre");
+	const inputDescripcion = document.getElementById("input-descripcion");
+	const inputGenero = document.getElementById("input-genero");
+	const inputAnio = document.getElementById("input-anio");
+	const inputDirector = document.getElementById("input-director");
+	const inputActores = document.getElementById("input-actores");
+	const inputCalificacion = document.getElementById("input-calificacion");
+
+	inputName.value = peli.name;
+	inputDescripcion.value = peli.descripcion;
+	inputGenero.value = peli.genero;
+	inputAnio.value = peli.anio;
+	inputDirector.value = peli.director;
+	inputActores.value = peli.actoresPrincipales;
+	inputCalificacion.value = peli.calificacion;
+
+	document.getElementById("card-detalle").style.display = "none";
+	document.getElementById("form-editar-peli").classList.remove("hidden");
+};
+
+const cancelarEdicion = () => {
+	document.getElementById("card-detalle").style.display = "flex";
+	document.getElementById("form-editar-peli").classList.add("hidden");
 };
